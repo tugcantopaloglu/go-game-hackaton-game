5c94f98beb3fd696d45b3de928c5625f9fa0f418
Tuğcan Topaloğlu <53763911+tugcantopaloglu@users.noreply.github.com>
2022-05-17 23:04:22 +0300

    level design

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,26 @@
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour
+{
+    
+    public image questItem;
+    public Color completedColor;
+    public Color activeColor;
+
+    public QuestArrow arrow;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player"){
+            FinishQuest();
+            Destroy(gameObject);
+        }
+    }
+
+    void FinishQuest(){
+
+    }
+}
+*/
