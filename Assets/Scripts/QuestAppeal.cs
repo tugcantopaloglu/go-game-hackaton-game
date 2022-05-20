e45c976bf3407301e14924819c64dbdf4a73c646
Tuğcan Topaloğlu <53763911+tugcantopaloglu@users.noreply.github.com>
2022-05-20 23:10:06 +0300

    quest done

--- Assets/Scripts/QuestAppeal.cs
+++ Assets/Scripts/QuestAppeal.cs
@@ -7,11 +7,16 @@ public class QuestAppeal : MonoBehaviour
 {
     [SerializeField] private GameObject[] buttons;
     [SerializeField] private GameObject[] questItems;
+    [SerializeField] private GameObject[] lights;
+
+    public static bool isGameOver = false;
 
     private void Start()
     {
 
         StartCoroutine(WaitForGameStart());
+        RenderSettings.fog = false;
+        RenderSettings.fogDensity = 0f;
 
     }
     private void FixedUpdate()
@@ -19,28 +24,63 @@ public class QuestAppeal : MonoBehaviour
         if (QuestTake.isQuest1Taken == true)
         {
             buttons[0].transform.DOMoveX(1700, 1);
+            questItems[0].SetActive(true);
         }
         if (QuestTake.isQuest2Taken == true)
         {
             buttons[1].transform.DOMoveX(1700, 1);
+            questItems[1].SetActive(true);
+            RenderSettings.fog = true;
+            RenderSettings.fogDensity = 0.1f;
         }
         if (QuestTake.isQuest3Taken == true)
         {
             buttons[2].transform.DOMoveX(1700, 1);
+            questItems[2].SetActive(true);
         }
         if (QuestTake.isQuest4Taken == true)
         {
             buttons[3].transform.DOMoveX(1700, 1);
+            questItems[3].SetActive(true);
         }
         if (QuestTake.isQuest1Taken == true && QuestTake.isQuest2Taken == true && QuestTake.isQuest3Taken == true && QuestTake.isQuest4Taken == true)
         {
             buttons[4].transform.DOMoveX(-220, 1);
         }
+
+        if (TrashPickUp.isQuest1Done == true)
+        {
+            buttons[0].transform.DOMoveX(-1700, 1);
+            lights[3].SetActive(true);
+        }
+        if (ItemTrigger.isQuest2Done == true)
+        {
+            buttons[1].transform.DOMoveX(-1700, 1);
+            lights[2].SetActive(true);
+        }
+        if (TrashPickUp.isQuest3Done == true)
+        {
+            buttons[2].transform.DOMoveX(-1700, 1);
+            lights[1].SetActive(true);
+        }
+        if (TrashPickUp.isQuest4Done == true)
+        {
+            buttons[3].transform.DOMoveX(-1700, 1);
+            lights[0].SetActive(true);
+        }
+        if (TrashPickUp.isQuest1Done == true && ItemTrigger.isQuest2Done == true && TrashPickUp.isQuest3Done == true && TrashPickUp.isQuest4Done == true)
+        {
+            lights[4].SetActive(true);
+            isGameOver = true;
+        }
+
     }
 
     IEnumerator WaitForGameStart()
     {
         yield return new WaitForSeconds(3);
         buttons[4].transform.DOMoveX(220, 1);
+
     }
+
 }
